824e0a556c81aa75e42068f458acf0330a18a5fc
jake.chu <jake.chu@gigabyte.com>
2021-04-28 15:21:47 +0800

    Init

---
+++ JobAPI/Controllers/WeatherForecastController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace JobAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly IBackgroundTaskQueue _taskQueue;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, IBackgroundTaskQueue taskQueue)
+        {
+            _logger = logger;
+            _taskQueue = taskQueue;
+        }
+
+        [HttpGet]
+        public async Task<string> Get(int sec=15)
+        {
+            _logger.LogInformation($"加入一個 {sec} 秒的 PowerShell 作業");
+            await _taskQueue.QueueBackgroundWorkItemAsync(async ct =>
+            {
+                var guid = Guid.NewGuid().ToString();
+
+                _logger.LogInformation($"開始一個 {sec} 秒的 PowerShell 作業 {guid}");
+            
+                await RunScript($"Start-Sleep -s {sec}", null);
+            
+                _logger.LogInformation($"完成一個 {sec} 秒的 PowerShell 作業 {guid}");
+            });
+            return $"加入一個 {sec} 秒的 PowerShell 作業";
+        }
+
+        /// <summary>
+        /// Runs a PowerShell script with parameters and prints the resulting pipeline objects to the console output. 
+        /// </summary>
+        /// <param name="scriptContents">The script file contents.</param>
+        /// <param name="scriptParameters">A dictionary of parameter names and parameter values.</param>
+        private async Task RunScript(string scriptContents, Dictionary<string, object> scriptParameters)
+        {
+            // create a new hosted PowerShell instance using the default runspace.
+            // wrap in a using statement to ensure resources are cleaned up.
+   
+            using (PowerShell ps = PowerShell.Create())
+            {
+                // specify the script code to run.
+                ps.AddScript(scriptContents);
+     
+                // specify the parameters to pass into the script.
+                if(scriptParameters != null)
+                    ps.AddParameters(scriptParameters);
+     
+                // execute the script and await the result.
+                var pipelineObjects = await ps.InvokeAsync().ConfigureAwait(false);
+     
+                // print the resulting pipeline objects to the console.
+                foreach (var item in pipelineObjects)
+                {
+                    Console.WriteLine(item.BaseObject.ToString());
+                }
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # PowerShellNetCoreQueue
 Power Shell Dot Net Core Background Task Queue
+
+info: JobAPI.QueuedHostedService[0]
+      Queued Hosted Service is running.
+info: Microsoft.Hosting.Lifetime[0]
+      Now listening on: https://localhost:5001
+info: Microsoft.Hosting.Lifetime[0]
+      Now listening on: http://localhost:5000
+info: Microsoft.Hosting.Lifetime[0]
+      Application started. Press Ctrl+C to shut down.
+info: Microsoft.Hosting.Lifetime[0]
+      Hosting environment: Development
+info: Microsoft.Hosting.Lifetime[0]
+      Content root path: JobAPI
+info: JobAPI.Controllers.WeatherForecastController[0]
+      加入一個 30 秒的 PowerShell 作業
+info: JobAPI.Controllers.WeatherForecastController[0]
+      開始一個 30 秒的 PowerShell 作業 155e8c0c-13dd-43ff-b506-d09eaf0a0b2b
+info: JobAPI.Controllers.WeatherForecastController[0]
+      加入一個 25 秒的 PowerShell 作業
+info: JobAPI.Controllers.WeatherForecastController[0]
+      加入一個 20 秒的 PowerShell 作業
+info: JobAPI.Controllers.WeatherForecastController[0]
+      完成一個 30 秒的 PowerShell 作業 155e8c0c-13dd-43ff-b506-d09eaf0a0b2b
+info: JobAPI.Controllers.WeatherForecastController[0]
+      開始一個 25 秒的 PowerShell 作業 c7f00d7c-7fd8-46e4-a777-3a921bf4c25c
+info: JobAPI.Controllers.WeatherForecastController[0]
+      完成一個 25 秒的 PowerShell 作業 c7f00d7c-7fd8-46e4-a777-3a921bf4c25c
+info: JobAPI.Controllers.WeatherForecastController[0]
+      開始一個 20 秒的 PowerShell 作業 471b2916-cd4e-4615-a90e-fdfced026ad0
+info: JobAPI.Controllers.WeatherForecastController[0]
+      完成一個 20 秒的 PowerShell 作業 471b2916-cd4e-4615-a90e-fdfced026ad0
